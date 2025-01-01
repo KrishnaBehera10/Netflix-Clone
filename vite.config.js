341b41bd7339aaebc25f667ff9e788d0320d126a
krishna behera <krishnakantbehera28@gmail.com>
2025-01-01 20:03:11 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/netflix-clone/",
   plugins: [react()],
-  base: "netflix-clone",
 });
